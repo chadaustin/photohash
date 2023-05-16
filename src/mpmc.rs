316f26971e550a51ade7b2e2bdaf19961b272cf4
Chad Austin <chad@chadaustin.me>
2023-05-16 06:19:40 +0000

    mpmc: two tests pass

--- src/mpmc.rs
+++ src/mpmc.rs
@@ -1,55 +1,64 @@
-use std::sync::{Arc, Mutex};
-
-//use async_std::sync::{Mutex, Condvar};
-use tokio::sync::Notify;
-
 use std::collections::VecDeque;
+use std::future::Future;
+use std::pin::Pin;
 use std::sync::atomic::AtomicUsize;
-use std::sync::mpsc::SendError;
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll, Waker};
 
 struct State<T> {
     queue: VecDeque<T>,
     tx_count: usize,
     rx_count: usize,
-}
-
-struct Queue<T> {
-    state: Mutex<State<T>>,
-    not_empty_or_no_sender: Notify,
+    rx_wakers: Vec<Waker>,
 }
 
 pub struct Sender<T> {
-    q: Arc<Queue<T>>,
+    state: Arc<Mutex<State<T>>>,
 }
 
 impl<T> Drop for Sender<T> {
     fn drop(&mut self) {
-        let state = self.q.state.lock();
+        let mut state = self.state.lock().unwrap();
         state.tx_count -= 1;
         if state.tx_count == 0 {
-            // TODO:
-            self.not_empty_or_no_sender.notify_waiters();
+            state.queue.clear();
+            // TODO: wake all the recv waiters
         }
+    }
+}
+
+#[must_use = "futures do nothing unless you `.await` or poll them"]
+pub struct Send<'a, T> {
+    sender: &'a Sender<T>,
+    value: Option<T>,
+}
+
+impl<'a, T> Unpin for Send<'a, T> {}
+
+#[derive(Debug)]
+pub struct SendError();
+
+impl<'a, T> Future for Send<'a, T> {
+    type Output = Result<(), SendError>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut state = self.sender.state.lock().unwrap();
+        state.queue.push_back(self.value.take().unwrap());
+        Poll::Ready(Ok(()))
     }
 }
 
 impl<T> Sender<T> {
-    pub async fn push(&self, value: T) -> Result<(), SendError<T>> {
-        let mut state = self.q.state.lock().unwrap();
-        let was_empty = state.queue.is_empty();
-        state.queue.push_back(value);
-        drop(state);
-
-        if was_empty {
-            self.q.not_empty.notify_one();
+    pub fn send(&self, value: T) -> Send<'_, T> {
+        Send {
+            sender: self,
+            value: Some(value),
         }
-
-        Ok(())
     }
 }
 
 pub struct Receiver<T> {
-    q: Arc<Queue<T>>,
+    state: Arc<Mutex<State<T>>>,
 }
 
 /*
@@ -61,30 +70,54 @@ impl<T> Drop for Receiver<T> {
 }
 */
 
-impl<T> Receiver<T> {
-    pub async fn pop(&self) -> Option<T> {
-        loop {
-            let mut state = self.q.state.lock().unwrap();
-            match state.queue.pop_front() {
-                Some(value) => return Some(value),
-                None => (),
+#[must_use = "futures do nothing unless you `.await` or poll them"]
+pub struct Recv<'a, T> {
+    receiver: &'a Receiver<T>,
+}
+
+impl<'a, T> Unpin for Recv<'a, T> {}
+
+#[derive(Debug)]
+pub struct RecvError();
+
+impl<'a, T> Future for Recv<'a, T> {
+    type Output = Option<T>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut state = self.receiver.state.lock().unwrap();
+        match state.queue.pop_front() {
+            Some(value) => Poll::Ready(Some(value)),
+            None => {
+                if state.tx_count == 0 {
+                    Poll::Ready(None)
+                } else {
+                    state.rx_wakers.push(cx.waker().clone());
+                    Poll::Pending
+                }
             }
-            drop(state);
-            self.q.not_empty.notified().await;
         }
     }
 }
 
+impl<T> Receiver<T> {
+    pub fn recv(&self) -> Recv<'_, T> {
+        Recv { receiver: self }
+    }
+}
+
 pub fn unbounded<T>() -> (Sender<T>, Receiver<T>) {
-    let q = Arc::new(Queue {
-        state: Mutex::new(State {
-            queue: VecDeque::new(),
-            tx_count: 1,
-            rx_count: 1,
-        }),
-        not_empty: Notify::new(),
-    });
-    (Sender { q: q.clone() }, Receiver { q })
+    let state = Arc::new(Mutex::new(State {
+        queue: VecDeque::new(),
+        tx_count: 1,
+        rx_count: 1,
+        rx_wakers: Vec::new(),
+    }));
+    (
+        Sender {
+            state: state.clone(),
+        },
+        Receiver { state },
+    )
 }
 
 #[cfg(test)]
@@ -92,16 +125,16 @@ mod tests {
     use crate::mpmc;
 
     #[tokio::test]
-    async fn push_and_pop() {
+    async fn send_and_recv() {
         let (tx, rx) = mpmc::unbounded();
-        tx.push(10).await.unwrap();
-        assert_eq!(Some(10), rx.pop().await);
+        tx.send(10).await.unwrap();
+        assert_eq!(Some(10), rx.recv().await);
     }
 
     #[tokio::test]
-    async fn pop_returns_none_if_sender_dropped() {
+    async fn recv_returns_none_if_sender_dropped() {
         let (tx, rx) = mpmc::unbounded();
         drop(tx);
-        assert_eq!(None as Option<()>, rx.pop().await);
+        assert_eq!(None as Option<()>, rx.recv().await);
     }
 }
