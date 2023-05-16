770355021c6dbb0e2c975b7a3562fcb71f35bcb5
Chad Austin <chad@chadaustin.me>
2023-05-16 04:36:38 +0000

    imagehash: mpmc compiles, one test passes

--- src/mpmc.rs
+++ src/mpmc.rs
@@ -1,9 +1,11 @@
+use std::sync::{Arc, Mutex};
+
+//use async_std::sync::{Mutex, Condvar};
 use tokio::sync::Notify;
-use std::sync::Arc;
-use async_std::sync::{Mutex, Condvar};
+
 use std::collections::VecDeque;
-use std::sync::mpsc::SendError;
 use std::sync::atomic::AtomicUsize;
+use std::sync::mpsc::SendError;
 
 struct State<T> {
     queue: VecDeque<T>,
@@ -13,7 +15,7 @@ struct State<T> {
 
 struct Queue<T> {
     state: Mutex<State<T>>,
-    has_item: Condvar,
+    not_empty_or_no_sender: Notify,
 }
 
 pub struct Sender<T> {
@@ -24,13 +26,24 @@ impl<T> Drop for Sender<T> {
     fn drop(&mut self) {
         let state = self.q.state.lock();
         state.tx_count -= 1;
+        if state.tx_count == 0 {
+            // TODO:
+            self.not_empty_or_no_sender.notify_waiters();
+        }
     }
 }
 
 impl<T> Sender<T> {
     pub async fn push(&self, value: T) -> Result<(), SendError<T>> {
-        let mut v = self.q.queue.lock().await;
-        v.push_back(value);
+        let mut state = self.q.state.lock().unwrap();
+        let was_empty = state.queue.is_empty();
+        state.queue.push_back(value);
+        drop(state);
+
+        if was_empty {
+            self.q.not_empty.notify_one();
+        }
+
         Ok(())
     }
 }
@@ -39,28 +52,39 @@ pub struct Receiver<T> {
     q: Arc<Queue<T>>,
 }
 
+/*
 impl<T> Drop for Receiver<T> {
     fn drop(&mut self) {
         let state = self.q.state.lock();
         state.rx_count -= 1;
     }
 }
+*/
 
 impl<T> Receiver<T> {
     pub async fn pop(&self) -> Option<T> {
-        let mut v = self.q.queue.lock().await;
-        Some(v.pop_front().unwrap())
+        loop {
+            let mut state = self.q.state.lock().unwrap();
+            match state.queue.pop_front() {
+                Some(value) => return Some(value),
+                None => (),
+            }
+            drop(state);
+            self.q.not_empty.notified().await;
+        }
     }
 }
 
 pub fn unbounded<T>() -> (Sender<T>, Receiver<T>) {
     let q = Arc::new(Queue {
-        queue: Mutex::new(VecDeque::new()),
-        has_item: Condvar::new(),
-        tx_count: AtomicUsize::new(1),
-        rx_count: AtomicUsize::new(1),
+        state: Mutex::new(State {
+            queue: VecDeque::new(),
+            tx_count: 1,
+            rx_count: 1,
+        }),
+        not_empty: Notify::new(),
     });
-    (Sender{q: q.clone()}, Receiver{q})
+    (Sender { q: q.clone() }, Receiver { q })
 }
 
 #[cfg(test)]
@@ -71,13 +95,13 @@ mod tests {
     async fn push_and_pop() {
         let (tx, rx) = mpmc::unbounded();
         tx.push(10).await.unwrap();
-        assert_eq!(10, rx.pop().await);
+        assert_eq!(Some(10), rx.pop().await);
     }
 
     #[tokio::test]
-    async fn pop_returns_None_if_sender_dropped() {
+    async fn pop_returns_none_if_sender_dropped() {
         let (tx, rx) = mpmc::unbounded();
         drop(tx);
-        assert_eq!(None, rx.pop().await);
+        assert_eq!(None as Option<()>, rx.pop().await);
     }
 }
