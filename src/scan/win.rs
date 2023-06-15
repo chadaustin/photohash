63ab715b3df7a676722ff77fd9d0d5d481b9d6ab
Chad Austin <chad@chadaustin.me>
2023-06-15 04:17:11 +0000

    imagehash: introduce a nice scanning iteration API

--- src/scan/win.rs
+++ src/scan/win.rs
@@ -5,6 +5,8 @@ use std::ffi::c_void;
 use std::ffi::OsString;
 use std::fs::Metadata;
 use std::io;
+use std::io::ErrorKind;
+use std::marker::PhantomData;
 use std::mem::MaybeUninit;
 use std::os::windows::ffi::OsStringExt;
 use std::path::Path;
@@ -33,48 +35,46 @@ use winapi::shared::ntdef::POBJECT_ATTRIBUTES;
 use winapi::shared::ntdef::PUNICODE_STRING;
 use winapi::shared::ntdef::PVOID;
 use winapi::shared::ntdef::UNICODE_STRING;
-use winapi::shared::ntstatus::STATUS_SUCCESS;
+use winapi::shared::ntstatus::*;
+use winapi::um::heapapi::GetProcessHeap;
+use winapi::um::heapapi::HeapAlloc;
+use winapi::um::heapapi::HeapFree;
 use winapi::um::winnt::FILE_ATTRIBUTE_NORMAL;
 use winapi::um::winnt::FILE_LIST_DIRECTORY;
 use winapi::um::winnt::FILE_SHARE_DELETE;
 use winapi::um::winnt::FILE_SHARE_READ;
 use winapi::um::winnt::FILE_SHARE_WRITE;
 use winapi::um::winnt::FILE_TRAVERSE;
+use winapi::um::winnt::HEAP_GENERATE_EXCEPTIONS;
 use winapi::um::winnt::SYNCHRONIZE;
+
+// TODO: This must be at least sizeof(FILE_DIRECTORY_INFORMATION) + 255 * 2
+const BUFFER_SIZE: usize = 65536;
+
+unsafe fn walloc(size: usize) -> *mut u8 {
+    HeapAlloc(GetProcessHeap(), HEAP_GENERATE_EXCEPTIONS, size) as *mut u8
+}
+
+unsafe fn wfree(p: *mut u8) {
+    _ = HeapFree(GetProcessHeap(), 0, p as PVOID);
+}
 
 pub struct DirectoryHandle {
     handle: HANDLE,
 }
 
-impl DirectoryHandle {}
-
-fn to_device_path<P: AsRef<Path>>(path: P) -> io::Result<Vec<u16>> {
-    let path = path.as_ref().canonicalize()?;
-    eprintln!("path = {}", path.display());
-    let mut v: Vec<u16> = path.as_os_str().encode_wide().collect();
-    if !v.starts_with(&[b'\\', b'\\', b'?', b'\\'].map(|c| c as u16)) {
-        // TODO: use InvalidFilename when stabilized?
-        return Err(io::Error::from(io::ErrorKind::InvalidInput));
+impl Drop for DirectoryHandle {
+    fn drop(&mut self) {
+        // TODO: Worth asserting if return value isn't STATUS_SUCCESS?
+        let result = unsafe { NtClose(self.handle) };
     }
-    // NtCreateFile requires absolute device paths.
-    // \??\ is an alias for \DosDevices\ that's faster to parse in
-    // Object Manager.
-    v[1] = b'?' as u16;
-    Ok(v)
 }
 
-pub fn windows_scan(paths: Vec<PathBuf>) -> mpmc::Receiver<(IMPath, Result<Metadata>)> {
-    let (meta_tx, meta_rx) = mpmc::unbounded();
+impl DirectoryHandle {
+    fn open<P: AsRef<Path>>(path: P) -> io::Result<DirectoryHandle> {
+        let mut path = to_device_path(path)?;
 
-    for path in paths {
-        let mut path = to_device_path(path).unwrap();
-
-        let mut unicode_string = UNICODE_STRING {
-            Length: 2 * path.len() as u16,
-            MaximumLength: 2 * path.len() as u16,
-            Buffer: path.as_mut_ptr(),
-        };
-        //() = unsafe { RtlInitUnicodeString(unicode_string.as_mut_ptr(), path.as_ptr()) };
+        let mut unicode_string = unicode_string(&mut path)?;
 
         let mut object_attributes = MaybeUninit::uninit();
         () = unsafe {
@@ -89,7 +89,6 @@ pub fn windows_scan(paths: Vec<PathBuf>) -> mpmc::Receiver<(IMPath, Result<Metad
 
         let mut io_status_block = MaybeUninit::zeroed();
         let mut handle = MaybeUninit::zeroed();
-
         let status: NTSTATUS = unsafe {
             NtCreateFile(
                 handle.as_mut_ptr(),
@@ -100,79 +99,187 @@ pub fn windows_scan(paths: Vec<PathBuf>) -> mpmc::Receiver<(IMPath, Result<Metad
                 FILE_ATTRIBUTE_NORMAL, // FileAttributes
                 FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
                 FILE_OPEN,
-                FILE_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_ALERT,
+                // TODO: Compare FILE_SYNCHRONOUS_IO_ALERT to FILE_SYNCHRONOUS_IO_NONALERT.
+                FILE_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT,
                 ptr::null_mut(),
                 0,
             )
         };
 
-        if status == STATUS_SUCCESS {
-            eprintln!("success!");
-        } else {
-            eprintln!("failure! {:#x}, {}", status as u32, unsafe {
-                io_status_block.assume_init().Information
-            });
-            panic!();
+        if status != STATUS_SUCCESS {
+            // TODO: use io_status_block.assume_init().Information?
+            return Err(to_error_kind(status).into());
         }
 
-        let mut io_status_block = MaybeUninit::zeroed();
-        const BUFFER_SIZE: u32 = 256 * 1024;
-        let mut buffer: MaybeUninit<[u8; BUFFER_SIZE as usize]> = MaybeUninit::uninit();
+        Ok(DirectoryHandle {
+            handle: unsafe { handle.assume_init() },
+        })
+    }
 
-        let status: NTSTATUS = unsafe {
-            NtQueryDirectoryFile(
-                handle.assume_init(),
-                ptr::null_mut(), // Event
-                None,            // ApcRoutine
-                ptr::null_mut(), // ApcContext
-                io_status_block.as_mut_ptr(),
-                buffer.as_mut_ptr() as *mut c_void,
-                BUFFER_SIZE,
-                FileDirectoryInformation,
-                FALSE,           // ReturnSingleEntry
-                ptr::null_mut(), // FileName
-                FALSE,           // RestartScan
-            )
+    fn entries(&self) -> Entries<'_> {
+        Entries {
+            handle: self.handle,
+            done: false,
+            buffer: unsafe { walloc(BUFFER_SIZE) },
+            next: None,
+            marker: PhantomData,
+        }
+    }
+}
+
+pub struct Entry<'a> {
+    name: &'a [u16],
+}
+
+impl<'a> Entry<'a> {
+    fn os_str(&self) -> OsString {
+        OsString::from_wide(&self.name)
+    }
+}
+
+pub struct Entries<'a> {
+    handle: HANDLE,
+    done: bool,
+    buffer: *mut u8,
+    next: Option<u32>,
+    marker: PhantomData<&'a ()>,
+}
+
+impl Drop for Entries<'_> {
+    fn drop(&mut self) {
+        unsafe { wfree(self.buffer) }
+    }
+}
+
+impl Entries<'_> {
+    fn next(&mut self) -> Option<io::Result<Entry<'_>>> {
+        if self.done {
+            return None;
+        }
+
+        let next = match self.next {
+            Some(next) => next,
+            None => {
+                eprintln!("calling");
+                let mut io_status_block = MaybeUninit::zeroed();
+                let status: NTSTATUS = unsafe {
+                    NtQueryDirectoryFile(
+                        self.handle,
+                        ptr::null_mut(), // Event
+                        None,            // ApcRoutine
+                        ptr::null_mut(), // ApcContext
+                        io_status_block.as_mut_ptr(),
+                        self.buffer as *mut c_void,
+                        BUFFER_SIZE as u32,
+                        FileDirectoryInformation,
+                        FALSE,           // ReturnSingleEntry
+                        ptr::null_mut(), // FileName
+                        FALSE,           // RestartScan
+                    )
+                };
+                match status {
+                    STATUS_SUCCESS => 0,
+                    STATUS_NO_MORE_FILES => {
+                        self.done = true;
+                        return None;
+                    }
+                    _ => {
+                        self.done = true;
+                        // TODO: use io_status_block.assume_init().Information?
+                        return Some(Err(to_error_kind(status).into()));
+                    }
+                }
+            }
         };
-        if status == STATUS_SUCCESS {
-            eprintln!("success! {}", unsafe {
-                io_status_block.assume_init().Information
-            });
-        } else {
-            eprintln!("failure! {:#x}, {}", status as u32, unsafe {
-                io_status_block.assume_init().Information
-            });
-            panic!();
-        }
 
-        let mut p = buffer.as_ptr() as *const FILE_DIRECTORY_INFORMATION;
-        eprintln!("first p = {:?}", p);
-        loop {
-            let ref e = unsafe { p.as_ref().unwrap() };
-            //eprintln!("p = {:?}", p);
-            //eprintln!("NextEntryOffset: {}", e.NextEntryOffset);
-            //eprintln!("FileNameLength: {}", e.FileNameLength);
-            let name = OsString::from_wide(unsafe {
+        let e: &FILE_DIRECTORY_INFORMATION = unsafe {
+            let p = self.buffer.offset(next as isize) as *const FILE_DIRECTORY_INFORMATION;
+            p.as_ref().unwrap()
+        };
+        self.next = if e.NextEntryOffset == 0 {
+            None
+        } else {
+            Some(next + e.NextEntryOffset)
+        };
+        return Some(Ok(Entry {
+            name: unsafe {
                 std::slice::from_raw_parts(
                     ptr::addr_of!(e.FileName) as *const u16,
                     (e.FileNameLength / 2) as usize,
                 )
-            });
-            eprintln!("{:?}", name);
+            },
+        }));
+    }
+}
 
-            if e.NextEntryOffset == 0 {
-                break;
-            }
-            p = unsafe {
-                ((p as *const u8).offset(e.NextEntryOffset as isize))
-                    as *const FILE_DIRECTORY_INFORMATION
-            };
+// ErrorKind is not very expressive. It will get slightly better with
+// https://github.com/rust-lang/rust/issues/86442 In the meantime, map
+// onto equivalents when possible. We could support a new Error type
+// with a lossy conversion into io::Error.
+fn to_error_kind(status: NTSTATUS) -> ErrorKind {
+    match status {
+        STATUS_SUCCESS => panic!("success"),
+        STATUS_ALERTED => ErrorKind::Interrupted,
+        STATUS_TIMEOUT => ErrorKind::TimedOut,
+        STATUS_PENDING => ErrorKind::WouldBlock,
+        //STATUS_REPARSE => ErrorKind::FilesystemLoop, // ??
+        //STATUS_MORE_ENTRIES => ;
+        //STATUS_NOT_ALL_ASSIGNED => ;
+        // ...
+        //STATUS_REPARSE_OBJECT => ErrorKind::FilesystemLoop, // ??
+        STATUS_INVALID_INFO_CLASS => ErrorKind::InvalidInput,
+        STATUS_INVALID_HANDLE => ErrorKind::InvalidInput,
+        STATUS_INVALID_PARAMETER => ErrorKind::InvalidInput,
+        // ...
+        // more?
+        // TODO:
+        _ => ErrorKind::Other,
+    }
+}
+
+fn to_device_path<P: AsRef<Path>>(path: P) -> io::Result<Vec<u16>> {
+    let path = path.as_ref().canonicalize()?;
+    let mut v: Vec<u16> = path.as_os_str().encode_wide().collect();
+    if !v.starts_with(&[b'\\', b'\\', b'?', b'\\'].map(|c| c as u16)) {
+        // TODO: use InvalidFilename when stabilized?
+        return Err(ErrorKind::InvalidInput.into());
+    }
+    // NtCreateFile requires absolute device paths.
+    // \??\ is an alias for \DosDevices\ that's faster to parse in
+    // Object Manager.
+    v[1] = b'?' as u16;
+    Ok(v)
+}
+
+fn unicode_string(path: &mut [u16]) -> io::Result<UNICODE_STRING> {
+    // The documentation
+    if path.len() >= 32767 {
+        // TODO: use InvalidFilename when stabilized?
+        return Err(ErrorKind::InvalidInput.into());
+    }
+
+    // We could use RtlInitUnicodeString but that's a function
+    // call and these fields have obvious meaning.
+    Ok(UNICODE_STRING {
+        Length: 2 * path.len() as u16,
+        MaximumLength: 2 * path.len() as u16,
+        Buffer: path.as_mut_ptr(),
+    })
+}
+
+pub fn windows_scan(paths: Vec<PathBuf>) -> mpmc::Receiver<(IMPath, Result<Metadata>)> {
+    let (meta_tx, meta_rx) = mpmc::unbounded();
+
+    for path in paths {
+        let handle = DirectoryHandle::open(path).unwrap();
+
+        let mut entries = handle.entries();
+        while let Some(info) = entries.next() {
+            let info = info.unwrap();
+            eprintln!("name: {}", info.os_str().to_string_lossy());
         }
-
-        unsafe { NtClose(handle.assume_init()) };
     }
 
     _ = meta_tx;
     meta_rx
-    //panic!("no return");
 }
