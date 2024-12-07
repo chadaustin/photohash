aa94a7959f9e51113834548722a46640ba9d292a
Chad Austin <chad@chadaustin.me>
2024-12-06 23:00:48 -0800

    add an untested function to compute MD5, SHA-1, SHA-256

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![allow(clippy::redundant_pattern_matching)]
 
 use clap::Parser;
+use digest::DynDigest;
 use photohash::iopool;
-use photohash::model::Hash32;
+use photohash::model::{ExtraHashes, Hash32};
 use std::io::Read;
 use std::path::PathBuf;
 
@@ -29,6 +30,44 @@ async fn compute_blake3(path: PathBuf) -> anyhow::Result<Hash32> {
             hasher.update(&buffer[..n]);
         }
         Ok(hasher.finalize().into())
+    })
+    .await
+}
+
+async fn compute_extra_hashes(path: PathBuf) -> anyhow::Result<ExtraHashes> {
+    // TODO: To save IO, this could be folded into compute_blake3 on
+    // the initial computation.
+
+    iopool::run_in_io_pool(move || {
+        let mut extra_hashes = ExtraHashes::default();
+
+        let mut hash_md5 = md5::Md5::default();
+        let mut hash_sha1 = sha1::Sha1::default();
+        let mut hash_sha256 = sha2::Sha256::default();
+        let mut hashers: [(&mut dyn DynDigest, &mut [u8]); 3] = [
+            (&mut hash_md5, extra_hashes.md5.get_or_insert_default()),
+            (&mut hash_sha1, extra_hashes.sha1.get_or_insert_default()),
+            (&mut hash_sha256, extra_hashes.sha256.get_or_insert_default()),
+        ];
+        let mut file = std::fs::File::open(path)?;
+        let mut buffer = [0u8; READ_SIZE];
+        loop {
+            let n = file.read(&mut buffer)?;
+            if n == 0 {
+                break;
+            }
+            let buffer = &buffer[..n];
+            for (h, _) in &mut hashers {
+                h.update(buffer);
+            }
+        }
+
+
+        for (h, dest) in &mut hashers {
+            h.finalize_into_reset(dest).expect("invalid buffer size");
+        }
+
+        Ok(extra_hashes)
     })
     .await
 }
